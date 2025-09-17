2dd8fdba0949d45b0b6c7f66448bd05686c7781c
Cody Platt <62489180+plattco@users.noreply.github.com>
2025-09-17 16:16:33 -0400

    Adding swagger to prod

--- MediaCollectionAPI/Program.cs
+++ MediaCollectionAPI/Program.cs
@@ -24,11 +24,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.UseCors("AllowVueApp");
